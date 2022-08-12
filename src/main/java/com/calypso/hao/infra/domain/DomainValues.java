350a2f924501a88e06da1a94cbf5ac3fb9906729
hitesh_kumar <hitesh_kumar@calypso.com>
2022-08-12 20:04:42 +0200

    first Commit
    Change-Id: I865c705d53c4786fe78172a975ff7b12b1ccaee0

---
+++ src/main/java/com/calypso/hao/infra/domain/DomainValues.java
@@ -0,0 +1,6 @@
+package com.calypso.hao.infra.domain;
+
+public class DomainValues {
+	public final static String SEPARATOR=";";
+	public static final String CSVSEPARATOR = ",";
+}
---
+++ src/main/java/com/calypso/hao/infra/spring/attribute.java
@@ -0,0 +1,5 @@
+package com.calypso.hao.infra.spring;
+
+public enum attribute {
+	BOX_REFERENCE, REFERENCE_FOLDER, TITLE
+}
